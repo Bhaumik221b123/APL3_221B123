764b61d2db08a7c484c88f0606c4bffb30a86da5
221B123 <221B123@juet.co.in>
2025-04-25 10:24:05 +0530

    lab 8

---
+++ LE08_221B123/IndianCrow.java
@@ -0,0 +1,11 @@
+public class IndianCrow implements Crow{
+    public void cry(){
+        System.out.println("Crwaaa Crwaa");
+    }
+    public void fly(){
+        System.out.println("Fly high in the sky");
+    }
+    public void eat(){
+        System.out.println("Eat pearls");
+    }
+}
